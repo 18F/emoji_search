e052d53ce74a9e613102b7b15de9aab06f2aa7f1
micahsaul <micah.saul@gsa.gov>
2015-11-05 10:37:35 -0800

    Initial checkin of slack emoji search

---
+++ README.md
@@ -0,0 +1,50 @@
+# Slack Emoji Search
+
+A commandline utility to search across Slack for messages that were reacted to with a specific emoji.
+
+## Installation
+
+Ensure Python and virtualenv are set up. If you've run the [laptop script](https://github.com/18F/laptop), you're all good.
+
+Clone the repo, set up a python virtualenvironment, and install requirements:
+
+    $ git clone thing ~/emoji_search
+    $ cd ~/emoji_search
+    $ mkvirtualenv emoji_search
+    $ pip install -r requirements.txt
+
+### Optionally make script executable outside of virtualenv
+
+If you always plan on running the script with the virtualenv activated, you can skip this step.
+
+Activate virtualenv if necessary:
+
+    $ workon emoji_search && cd ~/emoji_search
+
+Make script executable:
+
+    $ echo '#!'`which python`|cat - emoji_search.py > /tmp/out && mv /tmp/out emoji_search.py
+    $ chmod 755 emoji_search.py
+
+## Usage
+
+You will need a Slack API key. You can get this from the [Slack website](https://api.slack.com/web). The script expects the token to be in a file in the same directory, which will not be checked in to Github. To create it, run the following from the ~/emoji_search directory, subbing in your token and making sure to keep the quotes:
+
+    echo "API_TOKEN = '<MY-API-TOKEN>'" > api_token.py
+
+If you did not make the script executable in the optional step above use `python ~/emoji_search/emoji_search.py` in place of `~/emoji_search/emoji_search.py` below:
+
+To query for messages reacted to with the :evergreen_tree: emoji between Nov 2, 2015 and Oct 31, 2015 and write the output to a file called evergreen.txt in the current directory, run:
+
+    ~/emoji_search/emoji_search.py --emoji evergreen_tree \
+                                   --startdate 10-31-2015 \
+                                   --enddate 11-02-2015 \
+                                   --outfile evergreen.txt
+All flags are optional except for --emoji. If no destination file is provided, results will be written to the terminal.
+    
+## Public Domain
+18F's work on this project is in the worldwide [public domain](LICENSE.md).
+
+> This project is in the public domain within the United States, and copyright and related rights in the work worldwide are waived through the [CC0 1.0 Universal public domain dedication](https://creativecommons.org/publicdomain/zero/1.0/).
+>
+> All contributions to this project will be released under the CC0 dedication. By submitting a pull request, you are agreeing to comply with this waiver of copyright interest.
---
+++ emoji_search.py
@@ -0,0 +1,118 @@
+import click
+from pprint import pprint
+import re
+import requests
+import sys
+import time
+
+try:
+    from api_token import API_TOKEN
+except ImportError:
+    print('Please store API_TOKEN in api_token.py')
+    sys.exit(1)
+
+MESSAGE_ENDPOINT = 'https://slack.com/api/search.messages'
+USER_ENDPOINT = 'https://slack.com/api/users.info'
+CHANNEL_ENDPOINT = 'https://slack.com/api/channels.info'
+
+
+def lookup_username(userid):
+    '''Looks up username from Slack API based on userid
+
+    Expects:
+        userid: a valid userid of form <@XXX>
+
+    Returns:
+        string of username in form @YYYYY
+    '''
+    userid = re.sub('[<@|>]', '', userid)
+    payload = {'token': API_TOKEN, 'user': userid}
+    results = requests.get(USER_ENDPOINT, params=payload).json()
+    if results['ok']:
+        return '@' + results['user']['name']
+    return 'Unknown User'
+
+
+def lookup_channel(channelid):
+    '''Looks up channel name from Slack API based on channelid
+
+    Expects:
+        channelid: a valid userid of form <#XXX>
+
+    Returns:
+        string of username in form #YYYYY
+    '''
+    channelid = re.sub('[<#|>]', '', channelid)
+    payload = {'token': API_TOKEN, 'channel': channelid}
+    results = requests.get(CHANNEL_ENDPOINT, params=payload).json()
+    if results['ok']:
+        return '#' + results['channel']['name']
+    return 'Unknown Channel'
+
+
+def filter_by_date(messages, start, end):
+    try:
+        start = time.mktime(time.strptime(start, '%m-%d-%Y'))
+        end = time.mktime(time.strptime(end, '%m-%d-%Y'))
+    except:
+        raise ValueError('Invalid date')
+    return [m for m in messages if end > float(m['ts']) > start]
+
+
+def format_text(text):
+    '''Replaces all userids and channelids with plaintext names
+
+    Expects:
+        A string with 0 or more userids and channelids
+
+    Returns:
+        The same text with those ids replaced
+    '''
+    text = re.sub('<@\w*>', lambda x: lookup_username(x.group()), text)
+    text = re.sub('<#\w*>', lambda x: lookup_channel(x.group()), text)
+    return text
+
+
+@click.command()
+@click.option('--emoji', help=('Text name of the emoji to search, without '
+              'surrounding :s'))
+@click.option('--outfile', help=('Outputfile to write results to.'))
+@click.option('--startdate', help=('Date to begin search. Must be in '
+              'MM-DD-YYYY format.'), default='01-01-1999')
+@click.option('--enddate', help=('Date to end search. Must be in '
+              'MM-DD-YYYY format.'), default='12-12-2222')
+def get_messages(emoji, outfile, startdate, enddate):
+    if not emoji:
+        raise ValueError('Please supply an emoji name to search by')
+    print('Querying Slack api')
+    matches = []
+    paging = {'page': 0, 'pages': 99999}
+    while paging['page'] < paging['pages']:
+        payload = {'token': API_TOKEN,
+                   'query': 'has::' + emoji + ':',
+                   'page': paging['page'] + 1}
+        response = requests.get('https://slack.com/api/search.messages',
+                                params=payload).json()
+        if not response['ok']:
+            raise Exception('Query Failed!')
+        matches += response['messages']['matches']
+        paging = response['messages']['paging']
+        print('Fetched results page {} of {}'.format(paging['page'],
+                                                     paging['pages']))
+    print('Filtering by date')
+    matches = filter_by_date(matches, startdate, enddate)
+    print('Formatting results')
+    results = [{'ts': time.ctime(float(m['ts'])),
+                'username': m['username'],
+                'permalink': m['permalink'],
+                'text': format_text(m['text'])}
+               for m in matches]
+    if outfile:
+        print('Writing results to file')
+        with open(outfile, 'w') as f:
+            pprint(results, stream=f)
+    else:
+        pprint(results)
+
+if __name__ == '__main__':
+    get_messages()
