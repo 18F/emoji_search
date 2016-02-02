aba7a061955539e102c9d14414ea92b53d286907
Ian Lee <lee1001@llnl.gov>
2016-02-01 19:21:11 -0800

    Created setup.py file for package

---
+++ setup.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='slack-emoji-search',
+    version='0.0.1',
+    description='Search across Slack for messages with a specific emoji.',
+    author='Micah Saul',
+    author_email='micah.sual@gsa.gov',
+    url='https://github.com/18F/emoji_search',
+    license='CC0 1.0 Universal',
+    py_modules=['emoji_search'],
+    entry_points={
+        'console_scripts': [
+            'emoji_search = emoji_search:get_messages',
+        ],
+    },
+)
